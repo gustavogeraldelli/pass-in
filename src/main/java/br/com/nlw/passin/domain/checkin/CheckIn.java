6bfb2ae648c911a65448bd43722d1c6bf497500f
Gustavo <geraldellig@gmail.com>
2024-05-05 21:15:17 -0300

    add entities

---
+++ src/main/java/br/com/nlw/passin/domain/checkin/CheckIn.java
@@ -0,0 +1,31 @@
+package br.com.nlw.passin.domain.checkin;
+
+import br.com.nlw.passin.domain.attendee.Attendee;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "check_ins")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CheckIn {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "attendee_id", nullable = false)
+    private Attendee attendee;
+}
